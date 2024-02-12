62850227de7da6447f29dd370d81a417d936abfe
IvanGM <ivangm_01@hotmail.com>
2024-02-12 01:58:37 -0600

    feat: Agregada opción para detectar errores de json parser

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,6 +1,9 @@
 const errorHandler = (err, req, res, next) => {
-  // console.error(err)
-  const statusCode = res.statusCode || 500
+  let statusCode = res.statusCode || 500
+  // Para detectar errores de JSON parser
+  if (err instanceof SyntaxError && 'body' in err) {
+    statusCode = 400
+  }
   res.status(statusCode).json({
     message: err.message,
     stack: process.env.NODE_ENV === 'production' ? null : err.stack
